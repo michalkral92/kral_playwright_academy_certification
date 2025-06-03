0395a28d54e40e970741d5282ea7a8bb3697c9db
michalkral92 <michalkral92@gmail.com>
2025-06-03 17:02:37 +0200

    Editing DDT part 2

--- tests/data-driven-tests/account_balance_ddt.spec.ts
+++ tests/data-driven-tests/account_balance_ddt.spec.ts
@@ -8,10 +8,7 @@ import { CreateAccountApi } from "../../src/api/create_account_api.ts";
 test.describe("Data driven tests - create accounts with different account balances", () => {
   accountBalanceData.forEach((account) => {
     const { startBalance, type } = account;
-    test(`Create account with balance: ${startBalance} Kč`, async ({
-      page,
-      request,
-    }) => {
+    test(`Account balance: ${startBalance} Kč`, async ({ page, request }) => {
       const username = faker.internet.username();
       const emailDomain = "test.cz";
       const email = `${username}@${emailDomain}`;
@@ -20,32 +17,52 @@ test.describe("Data driven tests - create accounts with different account balanc
       const loginPage = new LoginPage(page);
       const loginApi = new LoginApi(request);
 
-      await loginPage
-        .goto()
-        .then((login) => login.clickRegistration())
-        .then((register) => register.typeUsername(username))
-        .then((register) => register.typePassword(password))
-        .then((register) => register.typeEmail(email))
-        .then((register) => register.clickRegister())
-        .then((login) =>
-          login.successRegistrationMessageHasText(
-            "🎉 Registrace úspěšná! Vítejte v TEG#B! 🎉"
-          )
+      await test.step("Unique user registration", async () => {
+        await loginPage
+          .goto()
+          .then((login) => login.clickRegistration())
+          .then((register) => register.typeUsername(username))
+          .then((register) => register.typePassword(password))
+          .then((register) => register.typeEmail(email))
+          .then((register) => register.clickRegister())
+          .then((login) =>
+            login.successRegistrationMessageHasText(
+              "🎉 Registrace úspěšná! Vítejte v TEG#B! 🎉"
+            )
+          );
+
+        console.log("Registered user:");
+        console.log("Username:", username);
+        console.log("Email:", email);
+        console.log("Password:", password);
+      });
+
+      await test.step("Creating account via API", async () => {
+        const loginResponse = await loginApi.loginViaApi(username, password);
+        const loginResponseBody = await loginResponse.json();
+        const token = loginResponseBody.access_token;
+        expect(loginResponse.status()).toBe(201);
+        expect(loginResponseBody.access_token).toBeDefined();
+        const accountApi = new CreateAccountApi(request);
+
+        const createAccountResponse = await accountApi.createAccountApi(
+          token,
+          startBalance,
+          type
         );
+        expect(createAccountResponse.status()).toBe(201);
+      });
 
-      const loginResponse = await loginApi.loginViaApi(username, password);
-      const loginResponseBody = await loginResponse.json();
-      const token = loginResponseBody.access_token;
-      expect(loginResponse.status()).toBe(201);
-      expect(loginResponseBody.access_token).toBeDefined();
-      const accountApi = new CreateAccountApi(request);
+      await test.step("Confirm account balance on FE", async () => {
+        const expectedStartBalance = `${startBalance.toFixed(2)}`;
 
-      const createAccountResponse = await accountApi.createAccountApi(
-        token,
-        startBalance,
-        type
-      );
-      expect(createAccountResponse.status()).toBe(201);
+        await loginPage
+          .goto()
+          .then((loginUser) => loginUser.login(username, password))
+          .then((dashboard) =>
+            dashboard.accountBalanceHasText(expectedStartBalance)
+          );
+      });
     });
   });
 });
